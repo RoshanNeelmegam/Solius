04801c7e0e195711269c59635126ac58e7e149e3
Roshan <roshanneelmegam@gmail.com>
2026-06-04 11:18:58 +0530

    Added the Scroll Area elements specifically the neighbours part

--- core.py
+++ core.py
@@ -5,9 +5,11 @@ class gui_window(QMainWindow):
         super().__init__()
         self.setWindowTitle('Solius Config Helper')
         self.resize(1300, 700)
+        self.neighbours = []
         self.init_left_pane()
         self.init_main_layout()
         self.init_central_widget()
+    
     
     def init_central_widget(self):
          # creating central widget
@@ -30,7 +32,7 @@ class gui_window(QMainWindow):
         self.init_addressFamily_box()
         # adding all 3 created widgets in the vertical layout
         self.leftVerticalLayout.addLayout(self.routerAs, stretch=0)
-        self.leftVerticalLayout.addWidget(self.neighboursBox, stretch=2)
+        self.leftVerticalLayout.addWidget(self.neighbourshipBox, stretch=2)
         self.leftVerticalLayout.addWidget(self.addressFamilyBox, stretch=2)
 
     def init_routerAS_box(self):
@@ -42,32 +44,50 @@ class gui_window(QMainWindow):
 
     def init_neighbourships_box(self):
         # creating Widget 2 in the left layout
-        self.neighboursBox = QGroupBox(title="Define Neighbourships")
+        self.neighbourshipBox = QGroupBox(title="Define Neighbourships")
         self.neighboursBoxVerticalLayout = QVBoxLayout()
-        # creating a Scroll Area
-        ScrollArea = QScrollArea()
-        ScrollAreaWidget = QWidget()
-        ScrollArea.setWidget(ScrollAreaWidget)
-        # creating a vertical layout withing the Scroll Area Widget
-        self.neighboursScrollAreaVerticalLayout = QVBoxLayout()
-        ScrollAreaWidget.setLayout(self.neighboursScrollAreaVerticalLayout)
-        self.neighboursBoxVerticalLayout.addWidget(ScrollArea, stretch=2)
-        self.neighboursBoxVerticalLayout.addWidget(QPushButton("Add Neighbourships"))
-        self.neighboursBox.setLayout(self.neighboursBoxVerticalLayout)
-        self.neighboursScrollAreaVerticalLayout.addWidget(QLabel("Test"))
-        self.neighboursScrollAreaVerticalLayout.addLayout(self.neighbour_defintion())
+        self.neighbourshipBox.setLayout(self.neighboursBoxVerticalLayout)
         
+        # building the innermost widget fully
+        self.scrollAreaWidgetLayout = QVBoxLayout()
+        self.scrollAreaWidgetLayout.addWidget(self.neighbour_defintion())
+        self.scrollAreaWidgetLayout.addWidget(self.neighbour_defintion())
+        self.scrollAreaWidgetLayout.addStretch()  # makes it absorb the left out space 
+        ScrollAreaWidget = QWidget()
+        ScrollAreaWidget.setLayout(self.scrollAreaWidgetLayout)
+        # building the next top layer
+        ScrollArea = QScrollArea()
+        ScrollArea.setWidget(ScrollAreaWidget)
+        ScrollArea.setWidgetResizable(True)
+        # adding scroll area to group box layout
+        self.neighboursBoxVerticalLayout.addWidget(ScrollArea)
+        self.addNeigbourshipsButton = QPushButton("Add Neighbourships")
+        self.addNeigbourshipsButton.clicked.connect(lambda: self.scrollAreaWidgetLayout.addWidget(self.neighbour_defintion()))
+        self.neighboursBoxVerticalLayout.addWidget(self.addNeigbourshipsButton) 
 
     def init_addressFamily_box(self):
         # creating Widget 3 in the left layout
         self.addressFamilyBox = QGroupBox(title="Configure Address-Families")
 
     def neighbour_defintion(self):
-        neighbourship_box = QVBoxLayout()
+        neighbour = QGroupBox()
+        layout = QVBoxLayout()
+        neighbour.setLayout(layout)
+        # defining neighbour ip
         neighbour_ip = QLineEdit()
         neighbour_ip.setPlaceholderText("Enter neighbour address. eg: 10.0.0.1")
-        neighbourship_box.addWidget(neighbour_ip)
-        return neighbourship_box
+        neighbour_ip_form = QFormLayout()
+        neighbour_ip_form.addRow("neighbour:", neighbour_ip)
+        # defining remote as
+        neighbour_as = QLineEdit()
+        neighbour_as.setPlaceholderText("Enter remote-as")
+        neighbour_as_form = QFormLayout()
+        neighbour_as_form.addRow("remote-as:", neighbour_as)
+
+        layout.addLayout(neighbour_ip_form)
+        layout.addLayout(neighbour_as_form)
+        self.neighbours.append(neighbour)
+        return neighbour
         
 
 
